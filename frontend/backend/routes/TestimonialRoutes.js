9f44ea2a93248430ebfa22e73011fd231cfde2b8
Delta student <rajputgangadhar351@gmail.com>
2025-08-27 19:06:33 +0530

    Remove backend package and associated routes, controllers, and utilities; delete unused images from public directory.

--- frontend/backend/routes/TestimonialRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllTestimonials, createTestimonial } = require('../controllers/TestimonialController');
-
-router.get('/', getAllTestimonials);
-
-router.post('/', createTestimonial);
-
-module.exports = router;
